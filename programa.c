8a4b6506e7240571cf5c67d609361ca395aa586a
Pedro Victor <pedro_jampa86@yahoo.com.br>
2013-06-29 14:46:09 -0300

    projeto de compilador c

---
+++ programa.c
@@ -0,0 +1,24 @@
+int main (){
+    int a;
+    int b=3;
+    int res;
+    float z = 6.6;
+
+    a = 10;
+    b = 5;
+
+    if (a>1)
+    {
+        return 5;
+    }
+    else
+    {
+        res = 2 * 2;
+        return 2;
+    }
+
+    while (res<100)
+    {
+        res = res + 1;
+    }
+}
